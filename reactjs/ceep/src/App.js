4ccf61e6cb1fda753243b62a78c7c8419f2e5880
Estevão Henrique <estevao@vibecriativa.com.br>
2022-02-18 22:24:55 -0300

    projeto reactjs

--- reactjs/ceep/src/App.js
+++ reactjs/ceep/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas/";
+import FormularioCadastro from "./components/FormularioCadastro/";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
 
-function App() {
-  return (
 
-    <div>
-      <h1>Meu app react</h1>
-    </div>
+  constructor() {
+    super();
+    this.state = {
+      notas: [],
+    }
+  }
 
-  );
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas: novoArrayNotas,
+    }
+    this.setState(novoEstado);
+  }
+
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
+      </section>
+    );
+  }
 }
 
 export default App;
